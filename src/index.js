6133ac6943cd747ff3b18b949a2609de268125e2
Eunjin Lee <reginalee@Eunjins-MacBook-Pro-8.local>
2020-12-28 16:12:15 +0900

    Add: 스타일 컴포넌트 초기셋팅 완료

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Routes from "./Routes";
-import "./styles/reset.scss";
+import GlobalStyle from "./styles/GlobalStyle";
+import { ThemeProvider } from "styled-components";
+import theme from "./styles/Theme";
 
-ReactDOM.render(<Routes />, document.getElementById("root"));
+ReactDOM.render(
+  <ThemeProvider theme={theme}>
+    <GlobalStyle />
+    <Routes />
+  </ThemeProvider>,
+  document.getElementById("root")
+);
---
+++ src/styles/Theme.js
@@ -0,0 +1,40 @@
+import { css } from "styled-components";
+
+const theme = {
+  fontSizeSupersmall: "12px",
+  fontSizeExtrasmall: "13px",
+  fontSizeSmall: "14px",
+  fontSizeRegular: "15px",
+  fontSizeMedium: "16px",
+  fontWeightLight: "200",
+  fontWeightRegular: "300",
+  fontWeightMedium: "400",
+  fontWeightBold: "600",
+  fontColorLightgray: "#bdbdbd",
+  fontColorGray: "#909090",
+  fontColorBlack: "#191919",
+  fontColorLighterblack: "#333",
+  fontColorLightblue: "#bbd6ed",
+  fontColorBlue: "#1e93f2",
+  primaryColor: "#ff395c",
+  bgColorGray: "#f7f7f7",
+  borderColor: "#f8f9fa",
+};
+
+export const flexCenter = css`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const flexJustCenter = css`
+  display: flex;
+  justify-content: center;
+`;
+
+export const flexAlignCenter = css`
+  display: flex;
+  align-items: center;
+`;
+
+export default theme;
