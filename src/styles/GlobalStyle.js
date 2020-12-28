e2ae10ff29ac47f9dc577d30779f40cf069bfedc
Eunjin Lee <reginalee@Eunjins-MacBook-Pro-8.local>
2020-12-28 16:12:15 +0900

    Add: 스타일 컴포넌트 초기셋팅 완료

---
+++ src/styles/GlobalStyle.js
@@ -0,0 +1,18 @@
+import { createGlobalStyle } from "styled-components";
+import { reset } from "styled-reset";
+
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+
+  * {
+    box-sizing: border-box;
+  }
+
+body {
+  font-family: 'proxima-nova', 'Noto Sans KR', sans-serif;
+  letter-spacing: -0.1px;
+}
+
+`;
+
+export default GlobalStyle;
