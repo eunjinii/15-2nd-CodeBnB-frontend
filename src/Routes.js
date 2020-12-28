b86c92d38fb8e0a8ed89fe0203432de51cff442a
Eunjin Lee <reginalee@Eunjins-MacBook-Pro-8.local>
2020-12-28 17:18:35 +0900

    Add: settings.json 추가

--- src/Routes.js
+++ src/Routes.js
@@ -1,21 +1,17 @@
-import React, { Component } from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from 'react-router-dom';
-import Home from './Pages/Home/Home';
-import RoomList from './Pages/RoomList/RoomList';
-import RoomDetail from './Pages/RoomDetail/RoomDetail';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./Pages/Home/Home";
+import RoomList from "./Pages/RoomList/RoomList";
+import RoomDetail from "./Pages/RoomDetail/RoomDetail";
 
 class Routes extends Component {
   render() {
     return (
       <Router>
         <Switch>
-          <Route exact path='/' component={Home} />
-          <Route exact path='/roomlist' component={RoomList} />
-          <Route exact path='/roomdetail' component={RoomDetail} />
+          <Route exact path="/" component={Home} />
+          <Route exact path="/roomlist" component={RoomList} />
+          <Route exact path="/roomdetail" component={RoomDetail} />
         </Switch>
       </Router>
     );
