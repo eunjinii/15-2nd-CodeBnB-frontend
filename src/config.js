7c8766c21d6d91a9b1995642898a60a31e2b84a3
TonyK <sunghk0901@gmail.com>
2021-01-16 16:27:50 +0900

    Add: 최종 취합 완료

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-const IP = "http://192.168.219.144:8000";
+const IP = "http://3.35.140.224:9000";
 
 export const KAKAO_KEY = "5177014e1f032e265934325b9b02886c";
 export const KAKAO_API = `${IP}/users/kakaologin`;
